f68b02bbbbbec47f81a259ed6d285860ce2e2287
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-19 15:14:29 -0500

    Load the admin page immediately

--- whos-coming/index.php
+++ whos-coming/index.php
@@ -13,12 +13,8 @@ define( __NAMESPACE__ . '\TEXT_DOMAIN', 'whos-coming' );
 define( __NAMESPACE__ . '\VERSION', '1.0.0' );
 define( __NAMESPACE__ . '\PROD', false );
 
-WP::add_action(
-	'plugins_loaded',
-	function() {
-		new AdminPage();
-	}
-);
+
+new AdminPage();
 
 WP::add_action(
 	'init',
